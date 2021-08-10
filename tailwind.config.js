7797ee1d0563f537f8a109583ebc8d1b3d9c2754
Mahdi Naderian <naderian.mahdi@gmail.com>
2021-08-10 20:11:46 +0430

    finished the grow section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,25 @@ module.exports = {
     extend: {
       colors: {
         'ocean': '#0b319c',
-        'goldish': '#ffb90a'
+        'goldish': '#ffb90a',
       }, 
       fontFamily: {
-        'montserrat': ['Montserrat']
-      }
+        'montserrat': ['Montserrat'],
+      },
+      backgroundImage: theme => ({
+        'amazon-advertising-gray': "url('../res/logos/amazon-advertising-gray.png')",
+        'amazon-advertising': "url('../res/logos/amazon-advertising.png')",
+        'amazon-gray': "url('../res/logos/amazon-gray.png')",
+        'amazon': "url('../res/logos/amazon.png')",
+        'jet-gray': "url('../res/logos/jet-gray.png')",
+        'jet': "url('../res/logos/jet.png')",
+        'shopify-gray': "url('../res/logos/shopify-gray.png')",
+        'shopify': "url('../res/logos/shopify.png')",
+        'walmart-gray': "url('../res/logos/walmart-gray.png')",
+        'walmart': "url('../res/logos/walmart.png')",
+        'woocommerce-gray': "url('../res/logos/woocommerce-gray.png')",
+        'woocommerce': "url('../res/logos/woocommerce.png')",
+      }),
     },
   },
   variants: {
